fa2e0a6f7e7a44f20afe58804d0bc9d26f78f501
pratik7103 <ppc712003@gmail.com>
2023-07-25 13:59:34 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,83 @@
+let currentQuestion = 0;
+let score = 0;
+let totalQuestion = questions.length;
+
+const quizContainer = document.querySelector("#quizContainer");
+const questionArea = document.querySelector("#questionArea")
+const theQuestion = document.querySelector("#question");
+const opt1 = document.querySelector("#opt1");
+const opt2 = document.querySelector("#opt2");
+const opt3 = document.querySelector("#opt3");
+const opt4 = document.querySelector("#opt4");
+const nextbtn = document.querySelector("#nextbtn");
+const finalbtn = document.querySelector("#finalbtn");
+const resultContainer = document.querySelector("#result");
+
+function startQuiz() {
+  const quizMasterInfo = document.querySelector("#quizMasterInfo");
+  const questionArea = document.querySelector("#questionArea");
+
+  quizMasterInfo.style.display = "none";
+  questionArea.style.display = "block";
+}
+
+
+function loadQuestion(questionIndex) {
+  let q = questions[questionIndex];
+  theQuestion.innerHTML = `${questionIndex + 1}. ${q.question}`;
+  opt1.innerHTML = q.options[0];
+  opt2.innerHTML = q.options[1];
+  opt3.innerHTML = q.options[2];
+  opt4.innerHTML = q.options[3];
+}
+loadQuestion(currentQuestion);
+
+function loadNextQuestion() {
+  let radios = document.querySelectorAll("input[name='option']");
+  let correctAnswer = questions[currentQuestion].answer;
+
+  for (let i = 0, radioLength = radios.length; i < radioLength; i++) {
+    if (radios[i].checked) {
+     
+      console.log(radios[i].value);
+      console.log(correctAnswer);
+
+      if (correctAnswer === i) {
+        console.log("correct answer");
+        score += 1;
+      }
+      radios[i].checked = false;
+      currentQuestion += 1;
+      loadQuestion(currentQuestion);
+    }
+  }
+}
+
+function loadResult() {
+  questionArea.style.display = "none";
+  resultContainer.style.display = "block";
+ 
+  if (score <= 0.6*totalQuestion){
+    resultContainer.innerHTML = `Your score is ${score} / ${totalQuestion} </br>
+    <bold>This is not impressive</bold>`;
+  };
+  if (score > 0.6*totalQuestion){
+    resultContainer.innerHTML = `Your score is ${score} / ${totalQuestion} </br>
+    <bold>Awesome</bold>`;
+  };
+  console.log(score);
+}
+
+nextbtn.onclick = function() {
+  if (currentQuestion < totalQuestion - 1) {
+    loadNextQuestion();
+  }
+
+  if (currentQuestion == totalQuestion - 1) {
+    nextbtn.style.display = "none";
+    finalbtn.style.display = "";
+  }
+
+  
+};
+finalbtn.onclick = loadResult;
---
+++ questions.js
@@ -0,0 +1,65 @@
+const questions=[
+    {
+        "id": 1,
+        "question": "Which country is known as the 'Land of the Rising Sun'?",
+        "options": ["China", "Japan", "South Korea", "Vietnam"],
+        "answer": 1
+      },
+      {
+        "id": 2,
+        "question": "Which planet is known as the 'Red Planet'?",
+        "options": ["Venus", "Mars", "Jupiter", "Neptune"],
+        "answer": 1
+      },
+      {
+        "id": 3,
+        "question": "Who is the author of the Harry Potter book series?",
+        "options": ["J.K. Rowling", "Stephen King", "George R.R. Martin", "Dan Brown"],
+        "answer": 0
+      },
+    {
+        "id": 4,
+        "question": "What is the largest desert in the world?",
+        "options": ["Gobi Desert", "Kalahari Desert", "Sahara Desert", "Atacama Desert"],
+        "answer": 2
+      },
+    {
+        "id": 5,
+        "question": "Which famous Japanese anime director created films like Spirited Away and My Neighbor Totoro?",
+        "options": ["Hayao Miyazaki", "Isao Takahata", "Mamoru Hosoda", "Makoto Shinkai"],
+        "answer": 0
+    },
+    {
+        "id":6,
+        "question":"What month and year was Avengers - End Game released?",
+        "options":["January 2018", "June 2019","April 2018", "April 2019"],
+        "answer": 0
+    },
+    {
+        "id": 7,
+        "question": "In which country would you find the famous landmark, the Eiffel Tower?",
+        "options": ["Italy", "Germany", "France", "Spain"],
+        "answer": 2
+      },
+      {
+        "id": 8,
+        "question": "What is the largest mammal in the world?",
+        "options": ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+        "answer": 1
+      },
+      {
+        "id": 9,
+        "question": "What is the chemical symbol for the element gold?",
+        "options": ["Au", "Ag", "Fe", "Cu"],
+        "answer": 0
+      },
+      {
+        "id": 10,
+        "question": "Who painted the famous artwork 'Mona Lisa'?",
+        "options": ["Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso", "Michelangelo"],
+        "answer": 0
+      },
+]
+
+
+
